fd16e45a06a1c376a6be27aa710672e07e039b17
sirjan tamang <sirjantmg99@gmail.com>
2024-05-20 15:53:38 +0545

    fixed production url

--- src/utils/api-service.utils.ts
+++ src/utils/api-service.utils.ts
@@ -13,9 +13,9 @@ export const getAuthorizationHeader = () => {
 };
 
 export const getClientHostName = () => {
-  // const localhost = `http://localhost:8000`;
-  // if (typeof window === "undefined") return localhost;
-  // if (process.env.NODE_ENV === "development") return localhost;
+  const localhost = `http://localhost:8000`;
+  if (typeof window === "undefined") return localhost;
+  if (process.env.NODE_ENV === "development") return localhost;
   return "http://54.242.165.66";
 };
 
